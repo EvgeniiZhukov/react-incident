45649d9bb63c569fbc1c363066edb379a079fd7f
EvgeniiZhukov <ev.zhykov@gmail.com>
2026-01-14 20:03:18 +0300

    My first commit before nre branch

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+function Footer() {
+    return <footer className="page-footer green lighten-4">
+          <div className="footer-copyright">
+            <div className="container">
+            © {new Date().getFullYear()} Copyright Text
+            <a className="grey-text text-lighten-4 right" href="https://github.com/EvgeniiZhukov/react-shop" target="_blank" rel="noreferrer">Repo</a>
+            </div>
+          </div>
+        </footer>
+
+}
+
+export {Footer}
