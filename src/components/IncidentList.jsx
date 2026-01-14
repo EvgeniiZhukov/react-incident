feb0809c965e363416cfa1fe8832617f41843b70
EvgeniiZhukov <ev.zhykov@gmail.com>
2026-01-14 21:11:05 +0300

    Added IncidentList

---
+++ src/components/IncidentList.jsx
@@ -0,0 +1,21 @@
+import { Incident } from "./Incident";
+
+function IncidentList() {
+    return (
+        <div className="list">
+            <Incident/>
+            <Incident/>
+            <Incident/>
+            <Incident/>
+            <Incident/>
+            <Incident/>
+            <Incident/>
+            <Incident/>
+            <Incident/>
+            <Incident/>
+            <Incident/>
+        </div>
+    )
+}
+
+export {IncidentList}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,5 +1,9 @@
+import { IncidentList } from "./IncidentList"
+
 function Main() {
-    return <main className='content'></main>
+    return <main className='content'>
+        <IncidentList/>
+    </main>
 }
 
 export {Main}
